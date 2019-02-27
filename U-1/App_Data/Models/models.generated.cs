8c41391ca104badd169df8fc135c1bc73d14b66d
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2019-02-27 13:52:16 +1300

    Commit for previous changes on U-1 umbraco starter kit

--- U-1/App_Data/Models/models.generated.cs
+++ U-1/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "b6f1d67b3a7b17e7")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
